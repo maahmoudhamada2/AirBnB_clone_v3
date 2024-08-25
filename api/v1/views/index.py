82e5eb42bc3817d0e76ed9160fed71613bc6e95c
maahmoudhamada <maahmoud.hamada@hotmail.com>
2024-08-25 08:31:05 +0300

    Adding: stats route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,3 +7,25 @@ from api.v1.views import app_views
 @app_views.route('/status')
 def status():
     return {"status": "OK"}
+
+
+@app_views.route('/stats')
+def stats():
+    """Method to show stat of objs"""
+    from models.state import State
+    from models.city import City
+    from models.amenity import Amenity
+    from models.review import Review
+    from models.place import Place
+    from models.user import User
+    from models import storage
+
+    obj_stats = {
+        'amenities': storage.count(Amenity),
+        'cities': storage.count(City),
+        'places': storage.count(Place),
+        'reviews': storage.count(Review),
+        'states': storage.count(State),
+        'users': storage.count(User)
+    }
+    return obj_stats
