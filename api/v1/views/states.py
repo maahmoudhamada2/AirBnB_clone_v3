462393fa90c30b525e526f3eff0c91a596b949aa
maahmoudhamada <maahmoud.hamada@hotmail.com>
2024-08-27 03:26:07 +0300

    Adding: State PUT, DELETE & POST routing

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -3,24 +3,70 @@
 from models import storage
 from models.state import State
 from api.v1.views import app_views
-from flask import jsonify, abort
+from flask import abort, request, jsonify
+
+# -------------------------------------------------------------------------------
 
 
-@app_views.route('/states', methods=['GET'])
+def bodyChecker(data):
+    """Method to check HTTP body response"""
+    if not isinstance(data, dict):
+        return "Not a JSON"
+    elif 'name' not in data:
+        return "Missing name"
+    else:
+        return None
+
+# -------------------------------------------------------------------------------
+
+
+@app_views.route('/states', strict_slashes=False, methods=['GET', 'POST'])
 def get_states():
     """Routing method to list states objs"""
     objs, objList = storage.all(State), []
-    for obj in objs.values():
-        objList.append(obj.to_dict())
-    return jsonify(objList)
+    if request.method == 'POST':
+        data = request.get_json()
+        mssg = bodyChecker(data)
+        if mssg:
+            return jsonify(mssg), 400
+        else:
+            s = State()
+            for key, value in data.items():
+                setattr(s, key, value)
+            s.save()
+            return s.to_dict(), 201
+    else:
+        for obj in objs.values():
+            objList.append(obj.to_dict())
+        return objList
+
+# -------------------------------------------------------------------------------
 
 
-@app_views.route('/states/<state_id>')
+@app_views.route('/states/<state_id>', methods=['GET', 'DELETE', 'PUT'])
 def single_state(state_id):
     """Routing method to get single state by id"""
+
     objs = storage.all(State)
     key = "{}.{}".format(State.__name__, state_id)
+    skipKeys = ['id', 'created_at', 'updated_at']
+
     if key not in objs:
         abort(404)
+    elif request.method == 'DELETE':
+        storage.delete(objs.get(key))
+        storage.save()
+        return {}, 200
+    elif request.method == 'PUT':
+        data = request.get_json()
+        bodyChecker(data)
+        obj = objs.get(key)
+        for key, value in data.items():
+            if key in skipKeys:
+                continue
+            else:
+                setattr(obj, key, value)
+        obj.save()
+        return obj.to_dict(), 200
     else:
-        return jsonify(objs.get(key).to_dict())
+        return objs.get(key).to_dict()
